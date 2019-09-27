77103d7fff50137f6981bb4aa558769f7f02c76b
SergiiSamoilenko <sergii.sako@gmail.com>
2019-09-27 18:18:02 +0300

    task1 finished

--- intro/start.cmd
+++ intro/start.cmd
@@ -1,3 +1,4 @@
-md target/classes
-rem javac ...
-rem java ...
+md target\classes
+javac -d target\classes src\main\java\com\epam\courses\java\fundamentals\intro\practice\task1\*
+java -classpath target\classes com.epam.courses.java.fundamentals.intro.practice.task1.Main
+
