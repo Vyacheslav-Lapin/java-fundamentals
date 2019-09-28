4fd0a3f4059a910154573e984136af155fc9be73
nikita0levchenko <36049814+nikita0levchenko@users.noreply.github.com>
2019-09-29 00:09:06 +0300

    Update start.cmd
    task1

--- intro/start.cmd
+++ intro/start.cmd
@@ -1,3 +1,4 @@
-md target/classes
-rem javac ...
-rem java ...
+mkdir target\classes
+javac -sourcepath . .\src\main\java\com\epam\courses\java\fundamentals\intro\practice\task1\Logic.java -d .\target\classes .\src\main\java\com\epam\courses\java\fundamentals\intro\practice\task1\Main.java
+java -cp .\target\classes com.epam.courses.java.fundamentals.intro.practice.task1.Main
+pause
