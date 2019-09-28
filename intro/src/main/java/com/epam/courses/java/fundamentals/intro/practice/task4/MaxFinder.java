647b6108db158b40def789918312d54b4ff939bd
nikita0levchenko <36049814+nikita0levchenko@users.noreply.github.com>
2019-09-29 00:15:29 +0300

    Update MaxFinder.java
    task4
    Даны действительные числа a1, a2 … an. Найти
    max(a1 + a2, a2 + a3, an-1 + an)

--- intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task4/MaxFinder.java
+++ intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task4/MaxFinder.java
@@ -13,6 +13,13 @@ public class MaxFinder {
 
   public double getMaximum() {
     //todo реализовать!
-    return 0;
+    double[] sumArray = new double[as.length - 1];
+    double max = Double.MIN_VALUE;
+    for(int i = 0; i < sumArray.length; i++)
+      sumArray[i] = as[i] + as[i + 1];
+    for(int i = 0; i < sumArray.length; i++)
+      if(sumArray[i] > max)
+        max = sumArray[i];
+    return max;
   }
 }
