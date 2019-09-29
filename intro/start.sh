301e47dd4cd58837580967675c34e8aff3627923
LakunDI <dshubn@gmail.com>
2019-09-29 20:30:46 +0300

    task 1, solution proposal

--- intro/start.sh
+++ intro/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p target/classes
-#javac ...
-#java ...
+javac src/main/java/com/epam/courses/java/fundamentals/intro/practice/task1/*.java -d target/classes/
+java -cp target/classes/  com.epam.courses.java.fundamentals.intro.practice.task1.Main
+
