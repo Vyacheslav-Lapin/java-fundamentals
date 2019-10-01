47311cc5bb90d6abde02e8120cdaffd60aaf7115
Irina Panova <irina_panova_1994@mail.ru>
2019-10-01 22:54:43 +0300

    Class defines function

--- intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task3/Table.java
+++ intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task3/Table.java
@@ -11,7 +11,7 @@ public class Table {
   public Table(double start, double end, double step) {
 
     if (end <= start)
-      throw new RuntimeException("End value shoult be bigger then start, but it's not.");
+      throw new RuntimeException("End value should be bigger than start, but it's not.");
 
     this.start = start;
     this.end = end;
@@ -27,8 +27,19 @@ public class Table {
   }
 
   public double[][] getTable() {
+    int rows = (int)((end - start + 1) / step);
+    int columns = 2;
+    double[][] mass = new double[rows][columns];
 
-    //todo реализовать
-    return new double[0][0];
+    double init = start;
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns - 1; j++) {
+        mass[i][j] = init;
+        mass[i][j + 1] = function(init);
+        init += step;
+      }
+    }
+    return mass;
   }
 }
