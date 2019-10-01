39a221cbe15151728670cbdef546561305119e28
zzSandyzz <tendersymphony@gmail.com>
2019-10-01 14:09:26 +0300

    solve 1.1

--- intro/start.sh
+++ intro/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mkdir -p target/classes
-#javac ...
-#java ...
+javac -d target/classes/intro src/main/java/com/epam/courses/java/fundamentals/intro/practice/task1/Logic.java src/main/java/com/epam/courses/java/fundamentals/intro/practice/task1/Main.java 
+java -cp target/classes/intro com.epam.courses.java.fundamentals.intro.practice.task1.Main
+
