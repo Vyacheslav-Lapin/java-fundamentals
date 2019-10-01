239e29afa5f5e201d59c945c997669dfdbf2b9bc
zzSandyzz <tendersymphony@gmail.com>
2019-10-01 14:19:34 +0300

    done

--- intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task4/MaxFinder.java
+++ intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task4/MaxFinder.java
@@ -5,19 +5,20 @@ package com.epam.courses.java.fundamentals.intro.practice.task4;
  */
 public class MaxFinder {
 
-  private double[] as;
+	private double[] as;
 
-  public MaxFinder(double... as) {
-    this.as = as;
-  }
+	public MaxFinder(double... as) {
+		this.as = as;
+	}
 
-  public double getMaximum() {
-	  double max=0.0; 
-	  for(int i =0; i<as.length -2; i++) {
-		  double curr = as[i]+as[i+1];
-		  if(curr>max) max= curr;
-	  }
-    
-    return max;
-  }
+	public double getMaximum() {
+		double max = 0.0;
+		for (int i = 0; i < as.length - 2; i++) {
+			double curr = as[i] + as[i + 1];
+			if (curr > max)
+				max = curr;
+		}
+
+		return max;
+	}
 }
