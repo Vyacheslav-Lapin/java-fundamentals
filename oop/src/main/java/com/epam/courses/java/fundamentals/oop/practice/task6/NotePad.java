75c090ae92773e793c971cd36c4789215b3dfdb6
nikita0levchenko <36049814+nikita0levchenko@users.noreply.github.com>
2019-10-02 18:42:07 +0300

    task6 class NotePad
    Add:
    *method show() - show all element's in notes
    *method's remove(int id) & remove(String title) - you can remove note with id/title with title from notes. If you have many element's with the same title's - all of them wil be remove
    *method's updateTitle(int id, String newTitle) & updateTitle(String oldTitle, String newTitle) - you can update title for element with id/title in notes. If you have many element's with the same title - title's all of them will be update.
    *method's updateBody(int id, String newBody) & updateBody(String oldTitle, String newTitle) - you can update body for element with id/title in notes. If you have many element's with the same title - body's all of them will be update.

--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
@@ -23,10 +23,86 @@ public class NotePad {
   }
 
   public Note addNote(String title, String body) {
+    try{
     Note note = new Note(index++).setTitle(title).setBody(body);
     return isPossibleToAdd() ? notes[note.getId()] = note :
                null; // throw new RuntimeException("It`s impossible to add the Note - array already have a maximum size!");
+    }catch (RuntimeException e){
+      e.printStackTrace();
+    }
   }
+  
+  public void remove(int id) {
+    if(id < 0 || id > notes.length - 1)
+      System.out.print("Wrong index!");
+    notes[id] = null;
+    notesSwapHelper(id);
+  }
+
+  public void remove(String title){
+    for(int i = 0; i < notes.length; i++)
+      if(notes[i] != null) {
+        if (notes[i].getTitle().equals(title)) {
+          notes[i] = null;
+          notesSwapHelper(i);
+        }
+      }
+  }
+
+  public void notesSwapHelper(int id){
+    for(int i = id; i < notes.length - 1; i++)
+      if(notes[i] == null && notes[i + 1] != null){
+        notes[i] = notes[i+1];
+        notes[i+1] = null;
+      }
+  }
+
+
+  public void show(){
+    for(int i = 0; i < this.getNotes().length; i ++)
+      if(this.getNote(i) != null)
+        System.out.println("|Id № " + (this.getNote(i).getId()) + " | Title: " + this.getNote(i).getTitle() + " | Body: " + this.getNote(i).getBody() + " |");
+      //else
+        //System.out.println("|Id №<empty> | Title: <empty> | Body: <empty> ");
+  }
+
+
+  public void updateTitle(int id , String newTitle){
+    if(id < 0 || id > notes.length)
+      System.out.print("Element not found. Incorrect index!");
+    notes[id].setTitle(newTitle);
+  }
+
+  public void updateTitle(String oldTitle, String newTitle){
+    boolean flag = false;
+    for(int i = 0; i < notes.length; i++)
+      if(notes[i] != null)
+      if(notes[i].getTitle().equals(oldTitle)) {
+        notes[i].setTitle(newTitle);
+        flag = true;
+      }
+    if(!flag)
+      System.out.println("Note with this title not found.");
+  }
+
+  public void updateBody(int id, String newBody){
+    if(id < 0 || id > notes.length)
+      System.out.print("Element not found. Incorrect index!");
+    notes[id].setBody(newBody);
+  }
+
+  public void updateBody(String oldTitle, String newBody){
+    boolean flag = false;
+    for(int i = 0; i < notes.length; i++)
+      if(notes[i] != null)
+      if(notes[i].getTitle().equals(oldTitle)) {
+        notes[i].setBody(newBody);
+        flag = true;
+      }
+    if(!flag)
+      System.out.println("Note with this title not found.");
+  }
+
 
   private boolean isPossibleToAdd() {
     return index < notes.length || hasGrew();
