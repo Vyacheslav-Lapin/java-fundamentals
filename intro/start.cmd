a1607757bb67357433f8c6e4a84b2c36e9fbf349
AA-Markov <markov_spb@mail.ru>
2019-10-08 18:00:21 +0300

    start

--- intro/start.cmd
+++ intro/start.cmd
@@ -1,3 +1,4 @@
-md target/classes
-rem javac ...
-rem java ...
+md target\classes
+javac -sourcepath ./src/main/java/com/epam/courses/java/fundamentals/intro/practice/task1
+-d target/classes/intro src/main/java/com/epam/courses/java/fundamentals/intro/practice/task1/Main.java
+java -classpath ./target/classes/intro com/epam/courses/java/fundamentals/intro/practice/task1/Main
