ad0b1226d65832698f974959600cd1636a08b198
AA-Markov <markov_spb@mail.ru>
2019-10-10 06:06:30 +0300

    3 task

--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/Note.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/Note.java
@@ -2,6 +2,13 @@ package com.epam.courses.java.fundamentals.oop.practice.task6;
 
 import lombok.experimental.NonFinal;
 
+/**
+ * Generates simple instances of Note
+ * Note includes Id as immutable identifier, and changeable string properties Title and Body
+ * @author Vyacheslav_Lapin
+ * @author Andrey_Markov
+ * @since 13.0
+ */
 public class Note {
 
   @NonFinal
@@ -10,44 +17,77 @@ public class Note {
   @NonFinal
   private String body;
 
+  /**
+   * Main identifier of instance (i.e. Primary Key)
+   * Value of Id is being set only by constructor
+  */
   private int id;
 
+  /**
+   *Creates a new Note with the specified Id
+   * @param id requires attention to set value that matches position of instance in {@link NotePad}
+   * @see #getId()
+   */
   public Note(int id) {
     this.id = id;
   }
 
+  /**
+   * @return Id of instance
+   * @see {@link #id}
+   */
   public int getId() {
     return id;
   }
-
+  /**
+   * @return Title of object
+   * @see {@link #title}
+   */
   public String getTitle() {
     return title;
   }
-
+  /**
+   * @param title   string new value for object title
+   */
   public Note setTitle(String title) {
     this.title = title;
     return this;
   }
-
+  /**
+   * @return Body of object
+   */
   public String getBody() {
     return body;
   }
+
+  /**
+   * @param body   string new value for object body
+   */
 
   public Note setBody(String body) {
     this.body = body;
     return this;
   }
 
+  /**
+   * @param o   object to compare with
+   */
   @Override
   public boolean equals(Object o) {
     return this == o || !(o == null || getClass() != o.getClass()) && id == ((Note) o).id;
   }
 
+  /**
+   *
+   */
   @Override
   public int hashCode() {
     return id;
   }
 
+  /**
+   *
+   */
   @Override
   public String toString() {
     return "Note{" + "title='" + title + '\'' +
--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
@@ -3,56 +3,113 @@ package com.epam.courses.java.fundamentals.oop.practice.task6;
 import java.util.Arrays;
 import lombok.experimental.NonFinal;
 
+/**
+ *
+ */
 public class NotePad {
-
+  /**
+   *
+   */
   private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
-
+  /**
+   *
+   */
   @NonFinal
   private Note[] notes;
-
+  /**
+   *
+   */
   @NonFinal
   private int index;
-
+  /**
+   *
+   */
+  public int getIndex() {
+    return index;
+  }
+  /**
+   *
+   */
   public NotePad(int initialCapacity) {
-    assert initialCapacity > 0;
+    if(initialCapacity <= 0) throw new RuntimeException("Invalid initialCapacity: capacity must be greater then 0!");
     this.notes = new Note[initialCapacity];
   }
-
+  /**
+   *
+   */
   public NotePad() {
     this(16);
   }
-
+  /**
+   * @param title
+   * @param body
+   */
   public Note addNote(String title, String body) {
     Note note = new Note(index++).setTitle(title).setBody(body);
-    return isPossibleToAdd() ? notes[note.getId()] = note :
-               null; // throw new RuntimeException("It`s impossible to add the Note - array already have a maximum size!");
+    if (isPossibleToAdd()) {
+      return notes[note.getId()] = note;
+    }
+    else {
+      throw new RuntimeException("It`s impossible to add the Note - array already have a maximum size!");
+    }
   }
-
+  /**
+   *
+   */
   private boolean isPossibleToAdd() {
     return index < notes.length || hasGrew();
   }
-
+  /**
+   *
+   */
   private boolean hasGrew() {
-
     int capacity = notes.length << 1; // * 2 with overflow insurance
-
     if (capacity - index < 0) // is previous operation has overflow as a result?
       if (index - MAX_ARRAY_SIZE <= 0)
         capacity = MAX_ARRAY_SIZE;
       else
         return false;
-
     notes = Arrays.copyOf(notes, capacity);
-
     return true;
   }
-
+  /**
+   * @param id
+   * @param newTitle
+   * @param newBody
+   */
+  public Note editNote(int id, String newTitle, String newBody){
+    return getNote(id).setTitle(newTitle).setBody(newBody);
+  }
+  /**
+   * @param id
+   */
   public Note getNote(int id) {
-    assert id >= 0 && id < index;
+    if (id < 0) throw new RuntimeException("Invalid note id: id must be greater then 0!");
+    if (id >= index) throw new RuntimeException("Invalid note id: Note with this id is out of this NotePad");
     return notes[id];
   }
-
+  /**
+   *
+   */
   public Note[] getNotes() {
     return Arrays.copyOf(notes, index); // better then clone for that case: result haven`t null`s at his tail
+  }
+  /**
+   *
+   */
+  public void viewAllNotes(){
+    for (Note a : getNotes()){
+      System.out.println(a.toString());
+    }
+  }
+  /**
+   * @param id
+   */
+  public void deleteNote(int id) {
+    for (int i = id; i < (index-1); i++){
+      notes[i] = new Note(i).setTitle(notes[i+1].getTitle()).setBody(notes[i+1].getBody());
+    }
+    notes[index-1] = null;
+    index--;
   }
 }
