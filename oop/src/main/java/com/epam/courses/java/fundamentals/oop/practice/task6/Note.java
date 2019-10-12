9c84dc1d2d26b0a3eed176147a38ff4fb46cf6b3
AA-Markov <markov_spb@mail.ru>
2019-10-12 14:30:09 +0300

    6 task (oop)

--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/Note.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/Note.java
@@ -3,8 +3,10 @@ package com.epam.courses.java.fundamentals.oop.practice.task6;
 import lombok.experimental.NonFinal;
 
 /**
- * Generates simple instances of Note
- * Note includes Id as immutable identifier, and changeable string properties Title and Body
+ * Class {@code Note} is a container for text notes.
+ * Note includes <code>id</code> as immutable identifier
+ * and changeable string properties <code>title</code> and <code>body</code>
+ * where <code>body</code> is very text note, and a title is a service tag.
  * @author Vyacheslav_Lapin
  * @author Andrey_Markov
  * @since 13.0
@@ -18,13 +20,14 @@ public class Note {
   private String body;
 
   /**
-   * Main identifier of instance (i.e. Primary Key)
-   * Value of Id is being set only by constructor
+   * Main identifier of instance (to be specific Primary Key)
+   * Value of <code>id</code> is being set only by constructor {@link Note#Note(int)}
   */
   private int id;
 
   /**
-   *Creates a new Note with the specified Id
+   * Class constructor
+   * Creates a new Note with the specified <code>id</code>
    * @param id requires attention to set value that matches position of instance in {@link NotePad}
    * @see #getId()
    */
@@ -33,35 +36,35 @@ public class Note {
   }
 
   /**
-   * @return Id of instance
-   * @see {@link #id}
+   * @return <code>id</code> of instance
    */
   public int getId() {
     return id;
   }
   /**
-   * @return Title of object
-   * @see {@link #title}
+   * @return <code>title</code> of object
    */
   public String getTitle() {
     return title;
   }
   /**
-   * @param title   string new value for object title
+   * @param title   string new value for <code>title</code>
+   * @return this instance
    */
   public Note setTitle(String title) {
     this.title = title;
     return this;
   }
   /**
-   * @return Body of object
+   * @return <code>body</code> of object
    */
   public String getBody() {
     return body;
   }
 
   /**
-   * @param body   string new value for object body
+   * @param body   string new value for <code>body</code>
+   * @return this instance
    */
 
   public Note setBody(String body) {
@@ -70,7 +73,9 @@ public class Note {
   }
 
   /**
-   * @param o   object to compare with
+   * Indicates whether some other object is "equal to" this one.
+   * @param o   the reference object with which to compare.
+   * @return  {@code true} if this object is the same as the obj argument; {@code false} otherwise.
    */
   @Override
   public boolean equals(Object o) {
@@ -78,7 +83,7 @@ public class Note {
   }
 
   /**
-   *
+   * Returns a hash code value for this object.
    */
   @Override
   public int hashCode() {
@@ -86,7 +91,7 @@ public class Note {
   }
 
   /**
-   *
+   * Returns a string representation of this object.
    */
   @Override
   public String toString() {
--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
@@ -4,45 +4,58 @@ import java.util.Arrays;
 import lombok.experimental.NonFinal;
 
 /**
- *
+ * Class {@code Notepad} is a storage for many instances of <code>Note</code>.
+ * NotePad includes array of notes, which are accessible by id.
+ * @author Vyacheslav_Lapin
+ * @author Andrey_Markov
+ * @since 13.0
  */
 public class NotePad {
-  /**
-   *
-   */
+
   private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
   /**
-   *
+   * Main field of class, object's contents
    */
   @NonFinal
   private Note[] notes;
+
   /**
-   *
+   * Keeps position and <code>id</code> of next <code>Note</code> to be generated
    */
   @NonFinal
   private int index;
+
   /**
-   *
+   * @return current <code>index</code> of this object
    */
   public int getIndex() {
     return index;
   }
+
   /**
-   *
+   * Class constructor
+   * @param initialCapacity   capacity of <code>notes</code>
    */
   public NotePad(int initialCapacity) {
     if(initialCapacity <= 0) throw new RuntimeException("Invalid initialCapacity: capacity must be greater then 0!");
     this.notes = new Note[initialCapacity];
   }
+
   /**
-   *
+   * Default constructor, set basic <code>notes</code> capacity of 16.
    */
   public NotePad() {
     this(16);
   }
+
   /**
-   * @param title
-   * @param body
+   * @param title  string value for <code>title</code>
+   * @param body   string value for <code>body</code>
+   * @return new instance of <code>Note</code> and adds this instance in <code>notes</code>
+   * on position of <code>index</code>
+   * @exception RuntimeException is thrown when array of <code>notes</code> has maximum size
+   * and new <code>Note</code> cannot be added
    */
   public Note addNote(String title, String body) {
     Note note = new Note(index++).setTitle(title).setBody(body);
@@ -53,12 +66,14 @@ public class NotePad {
       throw new RuntimeException("It`s impossible to add the Note - array already have a maximum size!");
     }
   }
+
   /**
    *
    */
   private boolean isPossibleToAdd() {
     return index < notes.length || hasGrew();
   }
+
   /**
    *
    */
@@ -72,6 +87,7 @@ public class NotePad {
     notes = Arrays.copyOf(notes, capacity);
     return true;
   }
+
   /**
    * @param id        required to select certain note
    * @param newTitle  string new title for note
@@ -80,20 +96,24 @@ public class NotePad {
   public Note editNote(int id, String newTitle, String newBody){
     return getNote(id).setTitle(newTitle).setBody(newBody);
   }
+
   /**
    * @param id
+   * @exception RuntimeException
    */
   public Note getNote(int id) {
     if (id < 0) throw new RuntimeException("Invalid note id: id must be greater then 0!");
     if (id >= index) throw new RuntimeException("Invalid note id: Note with this id is out of this NotePad");
     return notes[id];
   }
+
   /**
-   *
+   * @return
    */
   public Note[] getNotes() {
     return Arrays.copyOf(notes, index); // better then clone for that case: result haven`t null`s at his tail
   }
+
   /**
    *
    */
@@ -102,6 +122,7 @@ public class NotePad {
       System.out.println(a.toString());
     }
   }
+
   /**
    * @param id   required to select certain note
    */
