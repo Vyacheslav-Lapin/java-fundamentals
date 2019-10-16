8e9f2ea81706d3829180f2a0676bfbc56d69042e
cleve <clevernovel@gmail.com>
2019-10-16 16:13:13 +0300

    task1.4 max finder

--- intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task4/MaxFinder.java
+++ intro/src/main/java/com/epam/courses/java/fundamentals/intro/practice/task4/MaxFinder.java
@@ -1,5 +1,7 @@
 package com.epam.courses.java.fundamentals.intro.practice.task4;
 
+import static java.lang.System.out;
+
 /**
  * MaxFinder.
  */
@@ -11,8 +13,27 @@ public class MaxFinder {
     this.as = as;
   }
 
+  public static double getMaximum(double[] as) {
+    return new MaxFinder(as).getMaximum();
+  }
+
   public double getMaximum() {
     //todo реализовать!
-    return 0;
+    double max = 0;
+    for (int i = 1; i < as.length; i++) {
+      double temp = as[i] + as[i - 1];
+      out.println(temp);
+      if (max < temp)
+        max = temp;
+    }
+    return max;
+  }
+
+  public static void main(String... __) {
+    int n = (int) Math.round(Math.random() * 20);
+    double[] as = new double[n];
+    for (int i = 0; i < as.length; i++)
+      as[i] = Math.random() * 10;
+    out.println("Максимальное значение " + getMaximum(as));
   }
 }
