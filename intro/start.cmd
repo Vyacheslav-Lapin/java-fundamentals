c9a95f571df9c4d516f3e6448d95056e24f53a98
cleve <clevernovel@gmail.com>
2019-10-16 15:28:28 +0300

    task1.1 start cmd

--- intro/start.cmd
+++ intro/start.cmd
@@ -1,3 +1,8 @@
-md target/classes
-rem javac ...
-rem java ...
+mkdir target\classes
+javac -d target/classes -cp src/main/java src/main/java/com/epam/courses/java/fundamentals/intro/practice/task1/Main.java
+java -cp target/classes com/epam/courses/java/fundamentals/intro/practice/task1/Main
+
+
+
+
+
