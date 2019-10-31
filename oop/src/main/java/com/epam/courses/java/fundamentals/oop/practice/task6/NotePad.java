60bd6b3eb9c30dd25c23555ce87f55ff3f83cd1d
13thUnknown <misha34500@gmail.com>
2019-10-31 12:19:57 +0300

    Completed task1

--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task6/NotePad.java
@@ -28,6 +28,95 @@ public class NotePad {
                null; // throw new RuntimeException("It`s impossible to add the Note - array already have a maximum size!");
   }
 
+  public boolean editNote(String oldTitle, String newTitle){
+    return edit(findNote(oldTitle),newTitle,null);
+  }
+
+  public boolean editNote(String oldTitle, String oldBody, String newTitle){
+    return edit(findNote(oldTitle,oldBody),newTitle,null);
+  }
+
+  public boolean editNote(String oldTitle, String oldBody, String newTitle, String newBody){
+    return edit(findNote(oldTitle,oldBody),newTitle,newBody);
+  }
+
+  public boolean editNote(int index, String newTitle, String newBody){
+    return edit(findNote(index),newTitle,newBody);
+  }
+
+  private boolean edit(Note item,String title, String body){
+    if (item!=null){
+      if (title!=null)
+        item.setTitle(title);
+      if (body!=null)
+        item.setBody(body);
+      return true;
+    }
+    else return false;
+  }
+
+  public void showNotes(){
+    for(Note note:notes){
+      note.toString();
+    }
+  }
+
+  public Note findNote(String title){
+    for(Note item:notes){
+      if (item.getTitle()==title)
+        return item;
+    }
+    return null;
+  }
+
+  public Note findNote(int index){
+    for(Note item:notes){
+      if (item.getId()==index)
+        return item;
+    }
+    return null;
+  }
+
+  public Note findNote(String title, String body){
+    for(Note item:notes){
+      if (item.getTitle()==title && item.getBody()==body)
+        return item;
+    }
+    return null;
+  }
+
+  public boolean deleteNote(String title){
+    return delete(findNote(title));
+  }
+
+  public boolean deleteNote(String title, String body){
+    return delete(findNote(title, body));
+  }
+
+  public boolean deleteNote(int index){
+    return delete(findNote(index));
+  }
+
+  private boolean delete(Note item){
+    if (item==null) {
+      int id = item.getId();
+      Note temp;
+      notes[id] = null;
+      for (int i = id; i < index - 1; i++) {
+        if (notes[i + 1] == null) {
+          notes[i] = null;
+          break;
+        }
+        notes[i] = notes[i + 1];
+        notes[i].setId(notes[i].getId()-1);
+      }
+      index--;
+      return true;
+    }
+    else
+      return false;
+  }
+
   private boolean isPossibleToAdd() {
     return index < notes.length || hasGrew();
   }
