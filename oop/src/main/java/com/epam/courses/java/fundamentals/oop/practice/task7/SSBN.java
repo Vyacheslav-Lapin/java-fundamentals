e39d9131d06897a25c490154564319f9a7fa2532
AA-Markov <markov_spb@mail.ru>
2019-11-17 02:59:31 +0300

    OOP-7

--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task7/SSBN.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/task7/SSBN.java
@@ -1,6 +1,56 @@
 package com.epam.courses.java.fundamentals.oop.practice.task7;
 
-public class SSBN {
-  /**Разработайте класс АтомнаяЛодка, создайте внутренний класс – ДвигательДляАтомнойЛодки.
-  Создайте объект АтомнаяЛодка и “запустите его в плавание”.*/
+import lombok.SneakyThrows;
+import lombok.experimental.NonFinal;
+
+/**
+ * Разработайте класс АтомнаяЛодка, создайте внутренний класс – ДвигательДляАтомнойЛодки.
+ * Создайте объект АтомнаяЛодка и “запустите его в плавание”.
+ */
+
+public class SSBN implements Runnable {
+
+  @NonFinal
+  String name;
+
+  @NonFinal
+  private SSBN_PowerSupply engine;
+
+  @NonFinal
+  Type type;
+
+  public enum Type {MISSILE, ATTACKING;}
+
+  static class SSBN_PowerSupply {
+    @NonFinal
+    long totalPower;
+    @NonFinal
+    int numberOfReactors;
+
+    public SSBN_PowerSupply(long totalPower, int numberOfReactors) {
+      this.totalPower = totalPower;
+      this.numberOfReactors = numberOfReactors;
+    }
+  }
+
+   SSBN(String name, SSBN_PowerSupply engine, Type type) {
+    this.name = name;
+    this.engine = engine;
+    this.type = type;
+  }
+
+  @SneakyThrows
+  @Override
+  public void run() {
+    for (int i = 60; i > 0; i--) {
+      System.out.println("Vzzzz-Dr-Dr-Dr-bl-bl-bl");
+      Thread.sleep(500);
+    }
+  }
+
+  public static void main(String[] args) {
+    SSBN Moskva = new SSBN("Moskva", new SSBN_PowerSupply(5000, 2), Type.MISSILE);
+    Moskva.run();
+  }
 }
+
