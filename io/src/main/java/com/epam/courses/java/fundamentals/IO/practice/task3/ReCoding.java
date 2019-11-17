2dc41af525b8a04f52887b0062060874ae3e2c4d
AA-Markov <markov_spb@mail.ru>
2019-11-17 21:40:20 +0300

    task filled

---
+++ io/src/main/java/com/epam/courses/java/fundamentals/IO/practice/task3/ReCoding.java
@@ -0,0 +1,9 @@
+package com.epam.courses.java.fundamentals.IO.practice.task3;
+
+/**
+ * Дан файл, содержащий буквы текст на кириллице. Кодировка файла utf-8.
+ * Прочитайте информацию из файла и перепишите ее в файл в кодировкой utf-16.
+ */
+
+public class ReCoding {
+}
---
+++ strings/src/main/java/com/epam/courses/java/fundamentals/strings/practice/task1/CrazyLogger.java
@@ -0,0 +1,11 @@
+package com.epam.courses.java.fundamentals.strings.practice.task1;
+
+/**
+ * Необходимо создать класс CrazyLogger, ведущий журнал лога, используя как накопитель объект типа StringBuilder.
+ * Логгер должен содержать методы поиска определенной информации в таком логе
+ * [с возможностью вывода ее в поток ввода вывода]. Информацию логгер хранит в форматированном виде:
+ * dd-mm-YYYY : hh-mm – message
+ */
+
+public class CrazyLogger {
+}
