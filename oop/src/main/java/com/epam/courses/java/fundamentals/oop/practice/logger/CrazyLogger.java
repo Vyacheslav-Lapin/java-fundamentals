42c4b124ed9fba5201036b6e343d9613ffa6f40d
LakunDI <dshubn@gmail.com>
2019-11-27 11:11:14 +0300

    Task on Strings, "CrazyLogger", date and time edited

--- oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/logger/CrazyLogger.java
+++ oop/src/main/java/com/epam/courses/java/fundamentals/oop/practice/logger/CrazyLogger.java
@@ -1,9 +1,8 @@
 package com.epam.courses.java.fundamentals.oop.practice.logger;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,10 +11,8 @@ public class CrazyLogger {
   private StringBuilder logStore = new StringBuilder();
 
   public void log(String entry){
-    DateFormat dateFormat = new SimpleDateFormat("dd-mm-YYYY : hh-mm: ");
-    Date date = new Date();
-    String dateString=dateFormat.format(date);
-    logStore.append(dateString).append(entry).append("\n");
+    LocalDateTime date = LocalDateTime.now();
+    logStore.append(date.format(DateTimeFormatter.ofPattern("dd-mm-YYYY : hh-mm: "))).append(entry).append("\n");
   }
 
   public List<String> search(String pattern) {
