0719d91d0c31d7d78763911631713dbe7fc0bf5f
AA-Markov <markov_spb@mail.ru>
2019-12-01 19:52:16 +0300

    <technical commit>

--- threads/src/main/java/com/epam/courses/java/fundamentals/threads/practice/task2/PropFileNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.epam.courses.java.fundamentals.threads.practice.task2;
-
-import java.io.FileNotFoundException;
-
-public class PropFileNotFoundException extends FileNotFoundException {
-    public PropFileNotFoundException(String message) {
-        super(message);
-    }
-}
--- threads/src/main/java/com/epam/courses/java/fundamentals/threads/practice/task2/PropertyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.courses.java.fundamentals.threads.practice.task2;
-
-public class PropertyNotFoundException extends NullPointerException {
-    public PropertyNotFoundException(String message) {
-        super(message);
-    }
-}
